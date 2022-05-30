127145754b89d2320deaf179028e3109494275b0
Garrett Adams <garrett.adams52@gmail.com>
2022-05-30 18:19:03 -0400

    finished js

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,15 @@
 exports.palindrome = function(word) {
 
+    if(typeof(word)=== 'number')
+        word = word.toString()
+
+let temp = word.replace(/\W+/g,'').toLowerCase().split('').reverse().join('')
+word = word.replace(/\W/g,'').toLowerCase()
+
+
+if(temp === word)
+    return true
+
+return false
 };
+
